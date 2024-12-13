8295dce5cda72f36609a0a5c3f83cf7b08dbbad6
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2024-12-13 14:17:04 -0600

    remove REF function. Will be used in verifier operation seprately

--- timer_op.c
+++ timer_op.c
@@ -7,23 +7,14 @@
 #include "timer.h"
 
 // addition of external function interfaces to be used in test
-extern void COMPUTE_OP_REF(int m0, int n0, float *A_dist, float *B_dist, float *C_dist);
 
 extern void COMPUTE_OP_TEST(int m0, int n0, float *A_dist, float *B_dist, float *C_dist);
 
-extern void DISTRIBUTE_ALLOCATION_REF(int m0, int n0, float **A_dist, float **B_dist, float **C_dist);
-
 extern void DISTRIBUTE_ALLOCATION_TEST(int m0, int n0, float **A_dist, float **B_dist, float **C_dist);
-
-extern void DISTRIBUTE_DATA_REF(int m0, int n0, float *A_seq, float *B_seq, float *C_seq, float *A_dist, float *B_dist, float *C_dist);
 
 extern void DISTRIBUTE_DATA_TEST(int m0, int n0, float *A_seq, float *B_seq, float *C_seq, float *A_dist, float *B_dist, float *C_dist);
 
-extern void COLLECTION_REF(int m0, int n0, float *C_seq, float *C_dist);
-
 extern void COLLECTION_TEST(int m0, int n0, float *C_seq, float *C_dist);
-
-extern void FREE_MEMORY_REF(float *A_dist, float *B_dist, float *C_dist);
 
 extern void FREE_MEMORY_TEST(float *A_dist, float *B_dist, float *C_dist);
 
