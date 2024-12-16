0ef060bd20d6ca9a2ad48a39b4cdf52b86f9d860
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2024-12-16 13:49:05 -0600

    fix script spacing

--- dispatch_variables.sh
+++ dispatch_variables.sh
@@ -1,8 +1,8 @@
-BASELINE_VARIANT = "baseline.c"
+BASELINE_VARIANT="baseline.c"
 
-VARIANT_1 = "variant1.c"
-VARIANT_2 = "variant2.c"
-VARIANT_3 = "variant3.c"
+VARIANT_1="variant1.c"
+VARIANT_2="variant2.c"
+VARIANT_3="variant3.c"
 
 #Compiler flags
 CC=mpicc
