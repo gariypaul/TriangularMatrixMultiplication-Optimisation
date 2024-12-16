1dd15b86378ccc7754c8a03c79644645e9617235
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2024-12-16 12:58:58 -0600

    implemented timer operation

--- timer_op.c
+++ timer_op.c
@@ -16,7 +16,7 @@ extern void DISTRIBUTE_DATA_TEST(int m0, int n0, float *A_seq, float *B_seq, flo
 
 extern void COLLECTION_TEST(int m0, int n0, float *C_seq, float *C_dist);
 
-extern void FREE_MEMORY_TEST(float *A_dist, float *B_dist, float *C_dist);
+extern void FREE_MEMORY_TEST(int m0, int n0, float *A_dist, float *B_dist, float *C_dist);
 
 // fill created memory buffer with random values
 void fill_buffer_with_random_values(float *buffer, int num_elements)
@@ -129,5 +129,169 @@ void time_function_call(int num_trials, int num_runs, long *results, int m0, int
         {
             results[trial] = max_time;
         }
+    }
+}
+int scale_steps(int step, int dim)
+{
+    if (dim < 0)
+    {
+        return -1 * dim;
+    }
+    else
+    {
+        return dim * step;
+    }
+}
+int main(int argc, char *argv[])
+{
+    int rid;
+    int num_ranks;
+    int tag = 0;
+    int root_id = 0;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rid);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+
+    FILE *csv_file;
+
+    int num_trials = 10;
+    int num_runs = 1;
+
+    // Parameters for the test
+    int min_size;
+    int max_size;
+    int step_size;
+
+    int input_m0;
+    int input_n0;
+
+    if (argc == 1)
+    {
+        min_size = 16;
+        max_size = 1024;
+        step_size = 16;
+
+        input_m0 = 3;
+        input_n0 = 3;
+
+        csv_file = stdout;
+    }
+
+    else if (argc == 6 || argc = 7)
+    {
+        min_size = atoi(argv[1]);
+        max_size = atoi(argv[2]);
+        step_size = atoi(argv[3]);
+
+        input_m0 = atoi(argv[4]);
+        input_n0 = atoi(argv[5]);
+
+        // print to stdout if no file is specified
+        csv_file = stdout;
+
+        // if file specified then use the file
+        if (argc == 7)
+        {
+            csv_file = fopen(argv[6], "w");
+        }
+        else
+        {
+            csv_file = NULL;
+        }
+    }
+    else
+    {
+        printf("Usage: %s [min_size] [max_size] [step_size] [m0] [n0] [output_file]\n", argv[0]);
+        exit(1);
+    }
+
+    // use the root id to print the header on CSV file
+    if (rid == root_id)
+    {
+        fprintf(csv_file, "num_ranks, m0, n0, size, time\n");
+    }
+
+    for (int size = min_size; size <= max_size; size += step_size)
+    {
+        // scale the input sizes as per the step size
+        int m0 = scale_steps(step_size, input_m0);
+        int n0 = scale_steps(step_size, input_n0);
+
+        // buffer sizes
+        int A_seq_size = m0 * m0;
+        int B_seq_size = m0 * n0;
+        int C_seq_size = m0 * n0;
+
+        // allocate memory for sequential buffers
+        float *A_seq = (float *)malloc(A_seq_size * sizeof(float));
+        float *B_seq = (float *)malloc(B_seq_size * sizeof(float));
+        float *C_seq = (float *)malloc(C_seq_size * sizeof(float));
+
+        // fill buffers with random values using root_id
+        if (rid == root_id)
+        {
+            fill_buffer_with_random_values(A_seq, A_seq_size);
+            fill_buffer_with_random_values(B_seq, B_seq_size);
+            fill_buffer_with_random_values(C_seq, C_seq_size);
+        }
+
+        // allocate memory for distributed buffers
+        float *A_dist_test;
+        float *B_dist_test;
+        float *C_dist_test;
+
+        // distribute memory allocation
+        DISTRIBUTE_ALLOCATION_TEST(m0, n0, &A_dist_test, &B_dist_test, &C_dist_test);
+
+        // distribute data
+        DISTRIBUTE_DATA_TEST(m0, n0, A_seq, B_seq, C_seq, A_dist_test, B_dist_test, C_dist_test);
+
+        // allocate memory for results
+        long *results = (long *)malloc(num_trials * sizeof(long));
+
+        // perform test
+        time_function_call(num_trials, num_runs, results, m0, n0, A_dist_test, B_dist_test, C_dist_test);
+
+        //pick min in results
+        long min_time = pick_min_in_list(num_trials, results);
+
+        //get floating operation per second
+        long num_flops = m0 * m0 * n0 * 2; //multiply by two to factor in addition operation
+
+        // get throughput in GFLOPS
+        float throughput = (float)num_flops / (float)min_time;
+
+        //free results memory and set pointer to NULL to avoid dangling pointers
+        free(results);
+        results = NULL;
+
+        //collect the distributed data and write to sequential buffer
+        COLLECTION_TEST(m0, n0, C_seq, C_dist_test);
+
+        //free buffers
+        FREE_MEMORY_TEST(m0,n0, A_dist_test, B_dist_test, C_dist_test);
+
+        // print the results to the csv file
+        if (rid == root_id)
+        {
+            fprintf(csv_file, "%d, %d, %d, %d, %ld\n", num_ranks, m0, n0, size, throughput);
+        }
+
+        // free the sequential buffers and set pointers to NULL to avoid dangling pointers
+        free(A_seq);
+        free(B_seq);
+        free(C_seq);
+        A_seq = NULL;
+        B_seq = NULL;
+        C_seq = NULL;
+
+    }
+
+    // close the file if it was opened
+    if (csv_file != NULL)
+    {
+        fclose(csv_file);
     }
 }
